fcf02a329fa6919fb3dac46eaba3e13a466fb439
Evgeniy <evgeniy6183@gmail.com>
2024-07-10 15:11:06 +0300

    add-first_structure

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/constants.js';
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.use(cookieParser());
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '1mb',
+    }),
+  );
+
+  const PORT = env(ENV_VARS.PORT, 3000);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};

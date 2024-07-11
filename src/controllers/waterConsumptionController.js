18fd102f39119aa2a24f2dd012730ea49825da3c
RuslanGon <ruslan441006@gmail.com>
2024-07-11 22:14:18 +0300

    wer

---
+++ src/controllers/waterConsumptionController.js
@@ -0,0 +1,24 @@
+// Контроллеры для обработки запросов связанных с потреблением воды
+import { fetchDailyConsumptionService, fetchMonthlyConsumptionService } from '../services/waterConsumptionService.js';
+
+export const fetchDailyConsumption = async (req, res) => {
+  const date = req.params.date;
+
+  try {
+    const result = await fetchDailyConsumptionService(date);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const fetchMonthlyConsumption = async (req, res) => {
+  const month = req.params.month;
+
+  try {
+    const result = await fetchMonthlyConsumptionService(month);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
---
+++ src/db/WaterConsumptionData.js
@@ -0,0 +1,11 @@
+
+import DailyConsumption from './models/DailyConsumption.js';
+import MonthlyConsumption from './models/MonthlyConsumption.js';
+
+export const getDailyConsumption = async (date) => {
+  return await DailyConsumption.findOne({ date });
+};
+
+export const getMonthlyConsumption = async (month) => {
+  return await MonthlyConsumption.findOne({ month });
+};

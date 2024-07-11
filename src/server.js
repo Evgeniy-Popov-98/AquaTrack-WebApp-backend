887fb4b3f044ee89e7eb3c1965687599798bef94
Evgeniy <evgeniy6183@gmail.com>
2024-07-11 10:29:26 +0300

    add-error+notFaounf_Handler

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ import cookieParser from 'cookie-parser';
 import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/constants.js';
 
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
 export const setupServer = () => {
   const app = express();
 
@@ -27,6 +30,10 @@ export const setupServer = () => {
       limit: '1mb',
     }),
   );
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
 
   const PORT = env(ENV_VARS.PORT, 3000);
   app.listen(PORT, () => {

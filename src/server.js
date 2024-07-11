923d64018014e476384be4e561443a6235e542c3
RuslanGon <ruslan441006@gmail.com>
2024-07-11 21:53:34 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -1,49 +1,28 @@
+// Основной файл сервера Express
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/constants.js';
 
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
-import logger from './middleware/logger.js';
 import waterConsumptionRoutes from './routes/waterConsumptionRoutes.js';
 
-export const setupServer = () => {
-  const app = express();
+const app = express();
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
-  app.use(cors());
 
-  app.use(cookieParser());
 
-  app.use(
-    express.json({
-      type: ['application/json', 'application/vnd.api+json'],
-      limit: '1mb',
-    }),
-  );
+app.use('/api/water', waterConsumptionRoutes);
 
-  app.use(logger);
+app.use('*', notFoundHandler);
+app.use(errorHandler);
 
-  app.use('/water-consumption', waterConsumptionRoutes);
-
-  app.use('*', notFoundHandler);
-
-  app.use(errorHandler);
-
-  const PORT = env(ENV_VARS.PORT, 3000);
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
+const PORT = env(ENV_VARS.PORT, 3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

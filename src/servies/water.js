98544dbdd33a1e9e6d6ba9abbd4e671d8615da19
Evgeniy <evgeniy6183@gmail.com>
2024-07-15 14:57:28 +0300

    fix-start_server

--- src/servies/water.js
+++ src/servies/water.js
@@ -39,28 +39,23 @@ export const deleteWater = async (idRecordWater, userId) => {
 };
 
 export const fetchDailyService = async (date) => {
-  if (!validateDate(date)) {
-    throw new Error('Invalid date format');
-  }
-
-  const dailyConsumption = await getDailyConsumption(date);
-
-  if (!dailyConsumption) {
-    throw new Error('Data for the specified date was not found');
-  }
-
-  return formatResponse(date, dailyConsumption);
+  //   if (!validateDate(date)) {
+  //     throw new Error('Invalid date format');
+  //   }
+  //   const dailyConsumption = await getDailyConsumption(date);
+  //   if (!dailyConsumption) {
+  //     throw new Error('Data for the specified date was not found');
+  //   }
+  //   return formatResponse(date, dailyConsumption);
 };
 
 export const fetchMonthlyService = async (month) => {
-  if (!validateMonth(month)) {
-    throw new Error('Invalid month format');
-  }
-
-  const monthlyConsumption = await getMonthlyConsumption(month);
-  if (!monthlyConsumption) {
-    throw new Error('Data for the specified month was not found');
-  }
-
-  return formatResponse(month, monthlyConsumption);
+  //   if (!validateMonth(month)) {
+  //     throw new Error('Invalid month format');
+  //   }
+  //   const monthlyConsumption = await getMonthlyConsumption(month);
+  //   if (!monthlyConsumption) {
+  //     throw new Error('Data for the specified month was not found');
+  //   }
+  //   return formatResponse(month, monthlyConsumption);
 };

0f1b32b0b4cf96f2b2664148d6849647a57e9278
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-07-22 23:56:15 +0300

    Update userRoutes.js

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -31,55 +31,16 @@ import { validateGoogleOAuthSchema } from '../validation/validateGoogleOAuth.js'
 const router = Router();
 
 router.get('/total-users', getTotalUsers);
-
-router.post(
-  '/register',
-  validateBody(registerUserSchema),
-  ctrlWrapper(registerUserController),
-);
-router.post(
-  '/login',
-  validateBody(loginUserSchema),
-  ctrlWrapper(loginUserController),
-);
-
+router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
+router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 router.get('/current', authenticate, ctrlWrapper(getCurrentAccauntController));
-
 router.get('/:userId', ctrlWrapper(getFindtUserController));
-
-router.patch(
-  '/update',
-  authenticate,
-  validateBody(userSchema),
-  upload.single('avatar'),
-  ctrlWrapper(updateUserController),
-);
-
-router.post(
-  '/refresh-tokens',
-  authenticate,
-  ctrlWrapper(refreshTokensController),
-);
-
+router.patch('/update', authenticate, validateBody(userSchema), upload.single('avatar'), ctrlWrapper(updateUserController));
+router.post('/refresh-tokens', ctrlWrapper(refreshTokensController));
 router.post('/logout', authenticate, ctrlWrapper(logoutUserController));
-
-router.post(
-  '/send-reset-email',
-  validateBody(requestResetEmailSchema),
-  ctrlWrapper(sendResetPasswordEmailController),
-);
-
-router.post(
-  '/reset-password',
-  validateBody(resetPasswordSchema),
-  ctrlWrapper(resetPasswordController),
-);
-
+router.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(sendResetPasswordEmailController));
+router.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 router.post('/get-oauth-url', ctrlWrapper(getOAuthUrlController));
+router.post('/verify-google-oauth', validateBody(validateGoogleOAuthSchema), ctrlWrapper(verifyGoogleOAuthController));
 
-router.post(
-  '/verify-google-oauth',
-  validateBody(validateGoogleOAuthSchema),
-  ctrlWrapper(verifyGoogleOAuthController),
-);
 export default router;

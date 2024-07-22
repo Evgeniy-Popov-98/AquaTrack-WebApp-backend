3259eace33a3aa79b7eba6b4600778f72947bd48
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-07-22 12:13:39 +0300

    add_commit

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ export const setupServer = () => {
   app.use(
     cors({
       origin: 'https://aqua-track-web-app-frontend.vercel.app',
+      //   origin: ' http://localhost:4000',
       credentials: true,
     }),
   );
--- src/servies/water.js
+++ src/servies/water.js
@@ -1,16 +1,16 @@
 import { WaterCollection } from '../db/models/water.js';
 import { validateDate } from '../validation/dateValidation.js';
 import { formatResponse } from '../utils/formatResponse.js';
-import User from "../db/models/User.js";
+import User from '../db/models/User.js';
 
 export const createWater = async (userId, payload) => {
-  const water = await WaterCollection.create({userId, ...payload});
+  const water = await WaterCollection.create({ userId, ...payload });
   return water;
 };
 
-export const patchWater = async (userId, waterId, payload, options={}) => {
+export const patchWater = async (userId, waterId, payload, options = {}) => {
   const rawResult = await WaterCollection.findOneAndUpdate(
-    {userId, _id: waterId},
+    { userId, _id: waterId },
     payload,
     { new: true, includeResultMetadata: true, ...options },
   );
@@ -19,7 +19,8 @@ export const patchWater = async (userId, waterId, payload, options={}) => {
 
 export const deleteWater = async (userId, waterId) => {
   const water = await WaterCollection.findOneAndDelete({
-    userId, _id: waterId,
+    userId,
+    _id: waterId,
   });
   return water;
 };
@@ -45,14 +46,14 @@ export const fetchDailyService = async (userId, dateString) => {
     userId,
     createdAt: {
       $gte: startDate, // Дата більше або дорівнює початковій даті
-      $lt: endDate,    // Дата менше кінцевої дати
+      $lt: endDate, // Дата менше кінцевої дати
     },
   });
 
   // Перевірка, чи знайдені дані про споживання води
-  if (!dailyConsumption || dailyConsumption.length === 0) {
-    throw new Error('Data for the specified date was not found'); // Викидає помилку, якщо дані не знайдено
-  }
+  //   if (!dailyConsumption || dailyConsumption.length === 0) {
+  //     throw new Error('Data for the specified date was not found'); // Викидає помилку, якщо дані не знайдено
+  //   }
 
   // Пошук користувача для отримання денної норми води
   const user = await User.findById(userId);
@@ -62,10 +63,14 @@ export const fetchDailyService = async (userId, dateString) => {
   }
 
   // Обчислення загальної кількості спожитої води за день
-  const totalConsumption = dailyConsumption.reduce((total, record) => total + record.amountOfWater, 0);
+  const totalConsumption = dailyConsumption.reduce(
+    (total, record) => total + record.amountOfWater,
+    0,
+  );
 
   // Обчислення відсотка спожитої води відносно денної норми
-  const percentageOfDailyIntake = (totalConsumption / user.dailyWaterIntake) * 100;
+  const percentageOfDailyIntake =
+    (totalConsumption / user.dailyWaterIntake) * 100;
 
   // Форматування і повернення відповіді
   const formattedResponse = formatResponse(dateString, dailyConsumption);
@@ -94,9 +99,9 @@ export const fetchMonthlyService = async (userId, dateString) => {
     },
   });
 
-  if (!monthlyConsumption || monthlyConsumption.length === 0) {
-    throw new Error('Data for the specified month was not found');
-  }
+  //   if (!monthlyConsumption || monthlyConsumption.length === 0) {
+  //     throw new Error('Data for the specified month was not found');
+  //   }
 
   // Отримуємо користувача для доступу до денної норми води
   const user = await User.findById(userId);
@@ -112,12 +117,14 @@ export const fetchMonthlyService = async (userId, dateString) => {
   let totalMonthlyConsumption = 0;
 
   for (let day = 1; day <= daysInMonth; day++) {
-    const dayKey = `${year}-${month.padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    const dayKey = `${year}-${month.padStart(2, '0')}-${day
+      .toString()
+      .padStart(2, '0')}`;
     dailyConsumptionMap[dayKey] = 0;
   }
 
   // Заповнюємо об'єкт даними про споживання води
-  monthlyConsumption.forEach(record => {
+  monthlyConsumption.forEach((record) => {
     const dayKey = record.createdAt.toISOString().slice(0, 10);
     dailyConsumptionMap[dayKey] += record.amountOfWater;
     totalMonthlyConsumption += record.amountOfWater;

63032513e4907725e6a6fff42a6be446582b961f
Evgeniy <evgeniy6183@gmail.com>
2024-09-03 14:48:53 +0300

    add-fix

--- src/validation/water.js
+++ src/validation/water.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const createWaterSchema = Joi.object({
-  amountOfWater: Joi.number().min(50).max(300).required().messages({
+  amountOfWater: Joi.number().min(50).max(1500).required().messages({
     'number.base': 'Amount of water should be a number',
     'number.min': 'Amount of water should have at least {#limit} ml',
     'number.max': 'Amount of water should have at most {#limit} ml',
@@ -13,7 +13,7 @@ export const createWaterSchema = Joi.object({
 });
 
 export const updateWaterSchema = Joi.object({
-  amountOfWater: Joi.number().min(50).max(300).messages({
+  amountOfWater: Joi.number().min(50).max(1500).messages({
     'number.min': 'Amount of water should have at least {#limit} ml',
     'number.max': 'Amount of water should have at most {#limit} ml',
   }),
